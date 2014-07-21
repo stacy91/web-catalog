73e237ec9f62aa5c38f55f27cd74c7af72b4d74f
unknown <stst@SC0733.sam-solutions.net>
2014-07-21 12:38:56 +0300

    Working Hello

--- src/main/java/com/stas/controller/Home.java
+++ src/main/java/com/stas/controller/Home.java
@@ -1,16 +1,16 @@
-package com.stas.controller;  
-  
+package com.stas.controller;
+
 import org.springframework.stereotype.Controller;  
 import org.springframework.web.bind.annotation.RequestMapping;  
 import org.springframework.web.servlet.ModelAndView;  
   
 @Controller  
 public class Home {  
-    String message = "Stacy from controller!";  
+    String message = "Welcome from Home controller !";  
   
     @RequestMapping("/hello")  
     public ModelAndView showMessage() {  
         System.out.println("from controller");  
         return new ModelAndView("hello", "message", message);  
     }  
-} 
+}  
