955e9723fcd83de90b2882305846696f31f73323
unknown <stst@SC0733.sam-solutions.net>
2014-07-24 14:07:09 +0300

    New error!! Annotation exception, Arrival entity?

--- src/main/java/com/controller/management/Home.java
+++ src/main/java/com/controller/management/Home.java
@@ -1,11 +1,12 @@
 package com.controller.management;
 
 import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
 import org.springframework.stereotype.Controller;  
 import org.springframework.web.bind.annotation.RequestMapping;  
 import org.springframework.web.servlet.ModelAndView;  
-import org.hibernate.SessionFactory;
-import org.hibernate.Session;
+
 
 import com.entities.Device_Color;
 
